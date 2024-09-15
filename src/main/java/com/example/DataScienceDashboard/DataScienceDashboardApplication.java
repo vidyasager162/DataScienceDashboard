43f49cb5c80e70d2a7f9da211272ae73da37dc66
Vidyasager <vidyasager162@gmail.com>
2024-09-15 16:07:19 +0530

    Rebasing to Spring Boot

---
+++ src/main/java/com/example/DataScienceDashboard/DataScienceDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.example.DataScienceDashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataScienceDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataScienceDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DataScienceDashboard/DataScienceDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DataScienceDashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataScienceDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
